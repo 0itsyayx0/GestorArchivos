d87b6ca8ef86bb20e3ec7aa40208dea62c6384d5
Bryan Gabriel Cardozo Benavides <itsyayx@gmail.com>
2025-09-14 18:40:52 -0500

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from controllers.archivo_controller import gestor_archivos
-
-app = Flask(__name__)
-app.register_blueprint(gestor_archivos)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
